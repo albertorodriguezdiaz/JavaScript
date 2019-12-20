d348d886c961e54c805342f7dd36b4bccd53d9d4
albertorodriguezdiaz <alberto@efectovirtual.com>
2019-12-19 22:45:15 -0500

    Cargamos los archivos de prueba

---
+++ pruebas/app.js
@@ -0,0 +1,11 @@
+function imprimir(){
+    for(var i=0; i<8000; i++){
+        console.log("Imprimiendo");
+    }
+}
+
+ function presionanClick() {
+     console.log("Click en el Botón");
+ }
+
+imprimir();
